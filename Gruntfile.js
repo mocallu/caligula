b1adb2550292ffe6e35ef41d3a4f68d23583c674
Luã Mota <hi@mocallu.com>
2020-03-25 13:08:55 -0300

    Simple refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
   
-  /* Início - Carrega os módulos */
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-pug');
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -10,21 +9,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  /* Fim  - Carrega os módulos */
 
   grunt.initConfig({
     
-    /* Início - Lê o arquivo package e disponibiliza as configurações na variável pkg */
     pkg: grunt.file.readJSON('package.json'),
-    /* Fim - Lê o arquivo package e disponibiliza as configurações na variável pkg */
-
-    /* Início - Limpa diretórios */
     clean: {
       app: ['app']
     },
-    /* Fim - Limpa diretórios */
-
-    /* Início - Levanta a pasta app na porta 9000 */
     connect: {
       server: {
         options: {
@@ -48,24 +39,20 @@ module.exports = function(grunt) {
         }
       }
     },
-    /* Fim - Levanta a pasta app na porta 9000 */
-    
-    /* Início - Compila os arquivos .pug em .html */
     pug: {
       init: {
         options: {
           pretty: true,
           client: false,
           data: function(dest, src) {
-            //require('./locals.json');
             return {
-              pkg: grunt.file.readJSON('package.json'),
+              data: grunt.file.readJSON('src/data.json'),
             }
           }
         },
         files: [
           {
-            cwd: 'src',
+            cwd: 'src/pug/pages',
             src: '*.pug',
             dest: 'app',
             expand: true,
@@ -74,14 +61,11 @@ module.exports = function(grunt) {
         ]
       }
     },
-    /* Fim - Compila os arquivos .pug em .html */
-    
-    /* Início - Compila stylus em css */
     stylus: {
       init: {
         expand: true,
         cwd: 'src/stylus',
-        src: 'init.styl',
+        src: 'main.styl',
         dest: 'app/css',
         ext: '.css',
         options: {
@@ -89,9 +73,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    /* Fim - Compila stylus em css */
-    
-    /* Início - Aplica autoprefixer pra garantir mais compatibilidade do css com browsers mais antigos */
     autoprefixer: {
       css: {
           options: {
@@ -102,49 +83,36 @@ module.exports = function(grunt) {
           expand: true
       }
     },
-    /* Fim - Aplica autoprefixer pra garantir mais compatibilidade do css com browsers mais antigos */
-
-    /* Início - Comprime imagens */
     imagemin: {
       init: {
         files: [{
           expand: true,
           cwd: 'src/images',
-          src: ['**/*.{png,jpg,gif,ico}'],
+          src: ['**/*.{png,jpg,gif,ico,svg}'],
           dest: 'app/images'
         }]
       }
     },
-    /* Fim - Comprime imagens */
-
-
-    /* Início - Concatena todos os arquivos js em um único arquivo  */
     concat: {
       dist: {
-        src: ['src/libs/**/*.js', 'src/js/**/*.js'],
-        dest: 'app/js/init.js'
+        src: ['src/js/**/*.js'],
+        dest: 'app/js/main.js'
         //dest: 'app/js/<%= pkg.name %>-<%= pkg.version %>.js'
       }
     },
-    /* Fim - Concatena todos os arquivos js em um único arquivo  */
-
-    /* Início - Uglify no arquivo js principal */
     uglify: {
       init: {
         options: {
-          banner: '/*! <%= pkg.name %> - <%= pkg.version %> - Build: <%= grunt.template.today("dd/mm/yyyy H:M:ss") %>\n** Autor: <%= pkg.author %> \n*/'
+          banner: '/*! v<%= pkg.version %> - Build: <%= grunt.template.today("dd/mm/yyyy H:M:ss") %>\n*/'
         },
         files: [{
           // expand: true,
           // cwd: 'app',
-          src: ['app/js/init.js'],
-          dest: 'app/js/init.min.js'
+          src: ['app/js/main.js'],
+          dest: 'app/js/main.min.js'
         }]
       }
     },
-    /* Fim - Uglify no arquivo js principal */
-
-    /* Início - Escuta por mudanças nos arquivos, para todar o livereload */
     watch: {
       pug: {
         files: ['src/**/*.pug'],
@@ -175,17 +143,10 @@ module.exports = function(grunt) {
         }
       }
     }
-    /* Fim - Escuta por mudanças nos arquivos, para todar o livereload */
   });
 
-  /* Início - Captura os eventos do watch */
   grunt.event.on('watch', function(action, filepath, target) {
-    grunt.log.writeln(target + ': ' + filepath + ' foi ' + action);
+    grunt.log.writeln(target + ': ' + filepath + ' was ' + action);
   });
-  /* Fim - Captura os eventos do watch */
-
-  /* Início - Tarefas */
   grunt.registerTask('default', ['clean:app', 'pug', 'stylus', 'imagemin', 'concat', 'uglify', 'connect:server', 'watch']);
-  /* Fim - Tarefas */
-  
 };
